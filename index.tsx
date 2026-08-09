efaa667019be96352bfe079e4b3633b01ed47185
AdrianaCrz <eordonezc11@miumg.edu.gt>
2026-08-09 16:21:48 -0600

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,27 +1,72 @@
 import { View, Text, Image, Pressable, StyleSheet } from "react-native";
 
-export default function App(){
-return (
-  <View style={styles.contenedor}>
-<image  sourse {{}}
-  </View>
-)
+export default function App() {
+  return (
+    <View style={styles.contenedor}>
+
+      <Image
+        source={{
+          uri: "https://i.pinimg.com/736x/d5/3c/72/d53c720b99eaf76e49d9af973aaeb1f0.jpg"
+        }}
+        style={styles.foto}
+      />
+
+      <Text style={styles.nombre}>
+        Evelyn Adriana Ordóñez Cruz
+      </Text>
+
+      <Text style={styles.carrera}>
+        Ingeniería en Sistemas
+      </Text>
+
+      <Text style={styles.carnet}>
+        Carné: 0907-25-19505
+      </Text>
+
+      <Pressable style={styles.boton}>
+        <Text style={styles.botonTexto}>
+          VER PROYECTOS
+        </Text>
+      </Pressable>
+
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  contenedor: {    flex: 1,
+                   alignItems: "flex-start",
+                   justifyContent: "center",
+                   padding: 16,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  foto: {          width: 120,
+                   height: 120,
+                   borderRadius: 60,
+                   marginBottom: 16,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+
+  nombre: {        fontSize: 24,
+                   fontWeight: "bold",
+  },
+
+  carrera: {      fontSize: 16,
+                  color: "#66169c",
+                  marginTop: 4,
+  },
+
+  carnet: {       fontSize: 14,
+                  color: "#b972ca",
+                  marginTop: 4,
+  },
+
+  boton: {        marginTop: 20,
+                  backgroundColor: "#607bf1",
+                  padding: 12,
+                  borderRadius: 8,
+  },
+
+  botonTexto: {   color: "white",
+                  fontSize: 16,
   },
 });
